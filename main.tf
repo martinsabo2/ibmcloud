9f1124d07625ca05dfc5e11f8d71dbe6242e6b2e
Martin Sabo <martin.sabo1@ibm.com>
2023-05-15 14:19:46 +0200

    watson studion without module

--- main.tf
+++ main.tf
@@ -9,3 +9,18 @@ resource "ibm_resource_instance" "cos_instance" {
   plan              = "standard"
   location          = "global"
 }
+
+resource ibm_resource_instance studio_instance {
+  name              = "watson-studio1"
+  service           = "data-science-experience"
+  plan              = "professional-v1"
+  location          = "eu-de"
+  resource_group_id = ibm_resource_group.cos_group.id
+  tags              = []
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
