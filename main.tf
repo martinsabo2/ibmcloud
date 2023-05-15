e18c8090e604c7ac4efba4e095d6cdcc58377a70
Martin Sabo <martin.sabo1@ibm.com>
2023-05-15 14:13:05 +0200

    removed modules

--- main.tf
+++ main.tf
@@ -9,18 +9,3 @@ resource "ibm_resource_instance" "cos_instance" {
   plan              = "standard"
   location          = "global"
 }
-
-module "resource_group" {
-  source = "cloud-native-toolkit/resource-group/ibm"
-
-  resource_group_name = ibm_resource_group.cos_group.name
-  ibmcloud_api_key    = var.ibmcloud_api_key
-}
-
-module "watsonstudio" {
-  source = "github.com/ibm-garage-cloud/terraform-service-watsonstudio"
-
-  resource_group_name      = module.resource_group.name
-  resource_location        = var.region
-  name_prefix              = var.name_prefix
-}
